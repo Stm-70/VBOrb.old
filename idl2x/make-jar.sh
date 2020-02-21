3c48c0f66f023ec4009d9a3a8e9c29a7b1a740f5
Stm-70 <61235913+Stm-70@users.noreply.github.com>
2020-02-21 10:25:01 +0100

    Update make-jar.sh

--- idl2x/make-jar.sh
+++ idl2x/make-jar.sh
@@ -2,6 +2,8 @@
 
 	find gnu -name "*?.?*" | \
 		xargs jar -cvmf ./mboth/idl2vb/manifest.txt ../bin/IDL2VB.jar
+		
+	jar tf ../bin/IDL2VB.jar
     
 	find mboth/idl2vb mboth/util -name "*.class" | \
 		xargs jar uvf ../bin/IDL2VB.jar
